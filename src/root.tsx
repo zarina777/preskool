f537e9f43e88521bb5b6fec521961f761380f608
zarina777 <uktamovazarinabaratovna@gmail.com>
2024-08-27 16:32:58 +0500

    First commit

---
+++ src/root.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <h1>
+      Hi there i did not start it yet. Soon i will. Don't worry i will start
+      soon...
+    </h1>
+  </StrictMode>
+);
