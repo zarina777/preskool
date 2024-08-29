ddc87f2ede1da2bb924c44f068b8c13c28714666
zarina777 <uktamovazarinabaratovna@gmail.com>
2024-08-30 01:50:26 +0500

    sidebar somehow a bit changed

--- src/components/Layout/components/Sidebar/components/MultiNavs/index.tsx
+++ src/components/Layout/components/Sidebar/components/MultiNavs/index.tsx
@@ -1,19 +1,42 @@
+import { clsx } from "clsx";
+import { useRef, useState } from "react";
 import cn from "./style.module.scss";
 interface MultiNavProps {
   icon: string;
   name: string;
 }
 const MultiNav: React.FC<MultiNavProps> = ({ icon, name }) => {
+  const dropdown = useRef<HTMLUListElement | null>(null);
+  const [open, setOpen] = useState<boolean>(false);
   return (
-    <div className={cn.MultiNav}>
-      <div className={cn.wrap}>
-        <i className={icon}></i>
-        <span className={cn.name}>{name}</span>
+    <div className={cn.wrapper}>
+      <div
+        onClick={() => {
+          setOpen((prev: boolean) => (prev ? false : true));
+        }}
+        className={cn.MultiNav}
+      >
+        <div className={cn.wrap}>
+          <i className={icon}></i>
+          <span className={cn.name}>{name}</span>
+        </div>
+        <span className={cn.arrow}>
+          <i
+            className={clsx(
+              "fa-solid",
+              open ? "fa-chevron-down" : "fa-chevron-right"
+            )}
+          ></i>
+        </span>
       </div>
-      <span className={cn.arrow}>
-        <i className="fa-solid fa-chevron-right"></i>
-      </span>
-	  
+      <ul
+        style={{ display: open ? "block" : "none" }}
+        ref={dropdown}
+        className={cn.dropdown}
+      >
+        <li>All Students</li>
+        <li>Student List</li>
+      </ul>
     </div>
   );
 };
