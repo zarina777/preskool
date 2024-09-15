11c029edb2a6496b4facb4c48105227109e66061
zarina777 <uktamovazarinabaratovna@gmail.com>
2024-09-16 00:46:21 +0500

    xullas

--- src/components/HeroAdmin.tsx
+++ src/components/HeroAdmin.tsx
@@ -4,9 +4,7 @@ import { FiRefreshCcw } from "react-icons/fi";
 import { Link } from "react-router-dom";
 const HeroAdmin = () => {
   return (
-
-    <div className="content px-2 pt-[100px]" >
-
+    <div className="content px-2 pt-[100px]">
       {/* 1 */}
       <div className="d-md-flex d-block align-items-center justify-content-between mb-3">
         <div className="my-auto mb-2">
@@ -16,16 +14,28 @@ const HeroAdmin = () => {
               <li className="breadcrumb-item">
                 <Link to="/admin">Dashboard</Link>
               </li>
-              <li className="breadcrumb-item active" aria-current="page">Admin Dashboard</li>
+              <li className="breadcrumb-item active" aria-current="page">
+                Admin Dashboard
+              </li>
             </ol>
           </nav>
         </div>
         <div className="d-flex my-xl-auto right-content align-items-center flex-wrap">
           <div className="mb-2">
-            <Link to="/create-student" className="btn btn-primary d-flex align-items-center me-3"><FaRegSquarePlus /> &nbsp; Add New Student</Link>
+            <Link
+              to="/create-student"
+              className="btn btn-primary d-flex align-items-center me-3"
+            >
+              <FaRegSquarePlus /> &nbsp; Add New Student
+            </Link>
           </div>
           <div className="mb-2">
-            <Link to="/fees-detail" className="btn btn-light d-flex align-items-center">Fees Details</Link>
+            <Link
+              to="/fees-detail"
+              className="btn btn-light d-flex align-items-center"
+            >
+              Fees Details
+            </Link>
           </div>
         </div>
       </div>
@@ -33,35 +43,59 @@ const HeroAdmin = () => {
       {/* 2 */}
       <div className="row">
         <div className="col-md-12">
-
           <div className="card bg-dark">
             <div className="overlay-img">
-              <img src="assets/img/bg/shape-04.png" alt="img" className="img-fluid shape-01" />
-              <img src="assets/img/bg/shape-01.png" alt="img" className="img-fluid shape-02" />
-              <img src="assets/img/bg/shape-02.png" alt="img" className="img-fluid shape-03" />
-              <img src="assets/img/bg/shape-03.png" alt="img" className="img-fluid shape-04" />
+              <img
+                src="assets/img/bg/shape-04.png"
+                alt="img"
+                className="img-fluid shape-01"
+              />
+              <img
+                src="assets/img/bg/shape-01.png"
+                alt="img"
+                className="img-fluid shape-02"
+              />
+              <img
+                src="assets/img/bg/shape-02.png"
+                alt="img"
+                className="img-fluid shape-03"
+              />
+              <img
+                src="assets/img/bg/shape-03.png"
+                alt="img"
+                className="img-fluid shape-04"
+              />
             </div>
             <div className="card-body">
               <div className="d-flex align-items-xl-center justify-content-xl-between flex-xl-row flex-column">
                 <div className="mb-3 mb-xl-0">
                   <div className="d-flex align-items-center flex-wrap mb-2">
-                    <h1 className="text-white me-2">Welcome Back, Mr. Herald</h1>
-                    <Link to="profile.html" className="avatar avatar-sm img-rounded bg-gray-800 dark-hover"><FaRegEdit /></Link>
+                    <h1 className="text-white me-2">
+                      Welcome Back, Mr. Herald
+                    </h1>
+                    <Link
+                      to="profile.html"
+                      className="avatar avatar-sm img-rounded bg-gray-800 dark-hover"
+                    >
+                      <FaRegEdit />
+                    </Link>
                   </div>
                   <p className="text-white">Have a Good day at work</p>
                 </div>
-                <p className="text-white"><FiRefreshCcw />Updated Recently on 15 Jun
-                  2024</p>
+                <p className="flex">
+                  <FiRefreshCcw />
+                  <div className="d-inline-block text-white">
+                    Updated Recently on 15 Jun 2024
+                  </div>
+                </p>
               </div>
             </div>
           </div>
-
         </div>
       </div>
 
       {/* 3 */}
       <div className="row">
-
         {/* Students  */}
         <div className="col-xxl-3 col-sm-6 d-flex">
           <div className="card flex-fill animate-card border-0">
@@ -79,9 +113,13 @@ const HeroAdmin = () => {
                 </div>
               </div>
               <div className="d-flex align-items-center justify-content-between border-top mt-3 pt-3">
-                <p className="mb-0">Active : <span className="text-dark fw-semibold">3643</span></p>
+                <p className="mb-0">
+                  Active : <span className="text-dark fw-semibold">3643</span>
+                </p>
                 <span className="text-light">|</span>
-                <p>Inactive : <span className="text-dark fw-semibold">11</span></p>
+                <p>
+                  Inactive : <span className="text-dark fw-semibold">11</span>
+                </p>
               </div>
             </div>
           </div>
@@ -104,9 +142,13 @@ const HeroAdmin = () => {
                 </div>
               </div>
               <div className="d-flex align-items-center justify-content-between border-top mt-3 pt-3">
-                <p className="mb-0">Active : <span className="text-dark fw-semibold">254</span></p>
+                <p className="mb-0">
+                  Active : <span className="text-dark fw-semibold">254</span>
+                </p>
                 <span className="text-light">|</span>
-                <p>Inactive : <span className="text-dark fw-semibold">30</span></p>
+                <p>
+                  Inactive : <span className="text-dark fw-semibold">30</span>
+                </p>
               </div>
             </div>
           </div>
@@ -129,9 +171,13 @@ const HeroAdmin = () => {
                 </div>
               </div>
               <div className="d-flex align-items-center justify-content-between border-top mt-3 pt-3">
-                <p className="mb-0">Active : <span className="text-dark fw-semibold">161</span></p>
+                <p className="mb-0">
+                  Active : <span className="text-dark fw-semibold">161</span>
+                </p>
                 <span className="text-light">|</span>
-                <p>Inactive : <span className="text-dark fw-semibold">02</span></p>
+                <p>
+                  Inactive : <span className="text-dark fw-semibold">02</span>
+                </p>
               </div>
             </div>
           </div>
@@ -154,18 +200,20 @@ const HeroAdmin = () => {
                 </div>
               </div>
               <div className="d-flex align-items-center justify-content-between border-top mt-3 pt-3">
-                <p className="mb-0">Active : <span className="text-dark fw-semibold">81</span></p>
+                <p className="mb-0">
+                  Active : <span className="text-dark fw-semibold">81</span>
+                </p>
                 <span className="text-light">|</span>
-                <p>Inactive : <span className="text-dark fw-semibold">01</span></p>
+                <p>
+                  Inactive : <span className="text-dark fw-semibold">01</span>
+                </p>
               </div>
             </div>
           </div>
         </div>
-
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default HeroAdmin
+export default HeroAdmin;
